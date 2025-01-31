3b9ad793dc95237fd2838c5d576f436faba18224
magecrimson <109670124+magecrimson@users.noreply.github.com>
2025-01-31 13:42:39 -0500

    Try again

--- src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
+++ src/Miningcore/Blockchain/Bitcoin/BitcoinJob.cs
@@ -14,6 +14,7 @@ using NBitcoin.DataEncoders;
 using Newtonsoft.Json.Linq;
 using Contract = Miningcore.Contracts.Contract;
 using Transaction = NBitcoin.Transaction;
+using Miningcore.Logging;
 
 namespace Miningcore.Blockchain.Bitcoin;
 
@@ -28,6 +29,7 @@ public class BitcoinJob
     protected bool isPoS;
     protected string txComment;
     protected PayeeBlockTemplateExtra payeeParameters;
+    private static readonly ILogger logger = LogUtil.GetCurrentClassLogger();
 
     protected Network network;
     protected IDestination poolAddressDestination;
@@ -431,7 +433,8 @@ public class BitcoinJob
                 var blockBytes = stream.ToArray();
 
 			// 🚀 Print raw block before submission
-			logger.Info(() => $"Serialized Block: {blockBytes.ToHexString()}");
+			logger.Info($"Serialized Block: {blockBytes.ToHexString()}");
+
 
         return blockBytes;
         }
